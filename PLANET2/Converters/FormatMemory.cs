bf4400af91d0dfd29f3383dee278f384a5c4b91f
Jędrzej Skrodzki <jedrzej.skrodzki@gmail.com>
2017-11-13 23:53:54 +0100

    Init.

---
+++ PLANET2/Converters/FormatMemory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PLANET2.Converters
+{
+    class FormatMemory : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            long bytes = (long)value;
+            return $"{(bytes / 1024.00 / 1024.00).ToString ("0.##")} MB";
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PLANET2/ViewModels/ProcessesViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace PLANET2.ViewModels
+{
+    class ProcessesViewModel : INotifyPropertyChanged
+    {
+        private Process selected;
+        public Process Selected {
+            get => selected;
+            set {
+                selected = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Selected)));
+            }
+        }
+        public ObservableCollection<Process> Processes { get; } = new ObservableCollection<Process>();
+
+        public ProcessesViewModel()
+        {
+            var timer = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(1) };
+            timer.Tick += UpdateProcesses;
+            timer.Start();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void UpdateProcesses(object sender, EventArgs e)
+        {
+            Process tmp = Selected;
+            Processes.Clear();
+            foreach(var p in Process.GetProcesses())
+            {
+                Processes.Add(p);
+            }
+            if(tmp != null)
+                Selected = Processes.Where(x => x.Id == tmp.Id).First();
+        }
+
+    }
+}
