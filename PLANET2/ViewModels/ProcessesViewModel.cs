d8b4f15fe25a0691999371f76390616f170a0313
Jędrzej Skrodzki <jedrzej.skrodzki@gmail.com>
2017-11-14 11:50:28 +0100

    Change process priority.

--- PLANET2/ViewModels/ProcessesViewModel.cs
+++ PLANET2/ViewModels/ProcessesViewModel.cs
@@ -13,7 +13,13 @@ namespace PLANET2.ViewModels
 {
     class ProcessesViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private DispatcherTimer timer;
         private Process selected;
+        private int priority = 8;
+        
+
+        public ObservableCollection<Process> Processes { get; } = new ObservableCollection<Process>();
         public Process Selected {
             get => selected;
             set {
@@ -21,21 +27,54 @@ namespace PLANET2.ViewModels
                 PropertyChanged(this, new PropertyChangedEventArgs(nameof(Selected)));
             }
         }
-        public ObservableCollection<Process> Processes { get; } = new ObservableCollection<Process>();
-
+        public List<int> Priorities { get; } = new List<int> { 4, 8, 13, 24 }; 
+        public int Priority
+        {
+            get => priority;
+            set
+            {
+                priority = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Priority)));
+            }
+        }
         public ProcessesViewModel()
         {
-            var timer = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(1) };
+            timer = new DispatcherTimer() { Interval = TimeSpan.FromSeconds(1) };
             timer.Tick += UpdateProcesses;
             timer.Start();
         }
+       
+
+        public ICommand ChangePriority { get { return new RelayCommand(ChangePriorityExecute, () => true); } }
         public ICommand KillProcess { get { return new RelayCommand(KillProcessExecute, () => true); } }
         private void KillProcessExecute()
         {
             Selected.Kill();
             Selected = null;
         }
-        public event PropertyChangedEventHandler PropertyChanged;
+        private void ChangePriorityExecute()
+        {
+            timer.Stop();
+            switch (priority)
+            {
+                case 4:
+                    Selected.PriorityClass = ProcessPriorityClass.Idle;
+                    break;
+                case 8:
+                    Selected.PriorityClass = ProcessPriorityClass.Normal;
+                    break;
+                case 13:
+                    Selected.PriorityClass = ProcessPriorityClass.High;
+                    break;
+                case 24:
+                    Selected.PriorityClass = ProcessPriorityClass.RealTime;
+                    break;
+                default:
+                    Selected.PriorityClass = ProcessPriorityClass.Normal;
+                    break;
+            }
+            timer.Start();
+        }
 
         private void UpdateProcesses(object sender, EventArgs e)
         {
